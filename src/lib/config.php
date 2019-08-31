ede0160c31a1e1841979201bc06582911cb68790
richardbatstone <36573392+richardbatstone@users.noreply.github.com>
2019-08-31 09:01:42 +0100

    Update config.php

--- src/lib/config.php
+++ src/lib/config.php
@@ -1,7 +1,7 @@
 <?php 
 
 # The URL associated with your QnA instance's main page.
-$home = "/var/www/html/";
+$home = "/";
 
 # Source Whitelist. See http://www.qnamarkup.org/syntax/#remote
 # Provide a comma separated list of domains, and make sure to include the trailing slash (/).
